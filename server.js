612e3405f1d726c53a7f223dcf01e5fd76635f2f
hemanthgopisetty <hemanth.508136@gmail.com>
2023-09-08 16:07:07 +0530

    second commit get

--- server.js
+++ server.js
@@ -1,58 +1,46 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
 
-app.route('/bfhl')
-  .get((req, res) => {
-    res.json({ message: 'GET request to /bfhl received!' });
-  })
-  .post((req, res) => {
-    const requestData = req.body;
 
-    // 1. Status
-    const status = 'Success';
+app.post('/bfhl', (req, res) => {
+  const requestData = req.body.data;
 
-    // 2. User ID
-    const userId = requestData.userId;
 
-    // 3. College Email ID
-    const collegeEmailId = requestData.collegeEmailId;
+  const numbersArray = requestData.filter(item => !isNaN(item));
+  const alphabetsArray = requestData.filter(item => isNaN(item));
+  
 
-    // 4. College Roll Number
-    const collegeRollNumber = requestData.collegeRollNumber;
-
-    // 5. Array for numbers
-    const numbersArray = requestData.numbersArray || [];
-
-    // 6. Array for alphabets
-    const alphabetsArray = requestData.alphabetsArray || [];
-
-    // 7. Highest Alphabet in the input array of alphabets
-    let highestAlphabet = null;
-    if (alphabetsArray.length > 0) {
-      highestAlphabet = alphabetsArray.reduce((prev, current) => {
-        return (prev > current) ? prev : current;
-      });
+  let highestAlphabet = null;
+  for (const char of alphabetsArray) {
+    if (!highestAlphabet || char.toLowerCase() > highestAlphabet.toLowerCase()) {
+      highestAlphabet = char;
     }
+  }
 
-    const responseData = {
-      status,
-      userId,
-      collegeEmailId,
-      collegeRollNumber,
-      numbersArray,
-      alphabetsArray,
-      highestAlphabet,
-    };
+ 
+  const response = {
+    is_success: true,
+    user_id: 'john_doe_1709199',
+    email: 'john@xyz.com',
+    roll_number: 'ABCD123',
+    numbers: numbersArray,
+    alphabets: alphabetsArray,
+    highest_alphabet: highestAlphabet ? [highestAlphabet] : [],
+  };
 
-    res.json(responseData);
-  })
-  ;
+  res.json(response);
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.get('/bfhl',(req,res)=>{
+    const responseBody = { operation_code: 1 };
+    res.status(200).json(responseBody);
 })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
